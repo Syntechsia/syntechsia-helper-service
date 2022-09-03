f455ed51eaad16881b3c5ad6e54a4e8205433d95
Jafar <mjafar.shodiq@myboost.co>
2022-09-03 09:38:56 +0700

    first commit

---
+++ src/main/java/com/syntechsia/helper/service/syntechsiahelperservice/SyntechsiaHelperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.syntechsia.helper.service.syntechsiahelperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntechsiaHelperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyntechsiaHelperServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syntechsia/helper/service/syntechsiahelperservice/SyntechsiaHelperServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntechsia.helper.service.syntechsiahelperservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyntechsiaHelperServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
